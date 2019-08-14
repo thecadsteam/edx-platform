a976ff910a36d3fbe994dafcfccb18ddf13395a6
Ned Batchelder <ned@edx.org>
2019-08-14 15:55:23 -0400

    Reminder for cache configurability

--- cms/urls.py
+++ cms/urls.py
@@ -271,6 +271,7 @@ urlpatterns += [
 
 # API docs.
 urlpatterns += [
+    # TODO: cache_timeout should be 0 for devstack, long for production.
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^api-docs/$', schema_view.with_ui('swagger', cache_timeout=0)),
